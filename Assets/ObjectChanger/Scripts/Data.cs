b9b983130b3316843ca8c1550013c6332d49fed3
Adrian Galetov <adrian.galetov@bk.ru>
2024-04-05 12:54:28 +0700

    Create basic project

---
+++ Assets/ObjectChanger/Scripts/Data.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Data : MonoBehaviour
+{
+    public abstract void Hide();
+
+    public abstract void Show();
+
+    public abstract void Select();
+}
---
+++ Assets/ObjectChanger/Scripts/Input.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectorInput : MonoBehaviour
+{
+    [SerializeField] private KeyCode leftKey = KeyCode.LeftArrow;
+    [SerializeField] private KeyCode rightKey = KeyCode.RightArrow;
+    [SerializeField] private KeyCode selectKey = KeyCode.RightShift;
+
+    private Selector selector = null;
+
+    private void Awake()
+    {
+        selector = GetComponent<Selector>();
+    }
+
+    private void Update() {
+#if UNITY_STANDALONE
+        PCInput();
+#endif
+    }
+
+    private void PCInput() {
+        if(Input.GetKeyDown(leftKey)){
+            selector.PreviousItem();
+        } else if(Input.GetKeyDown(rightKey)){
+            selector.NextItem();
+        } else if(Input.GetKeyDown(selectKey)){
+            selector.SelectItem();
+        }
+    }
+}
